abeae70a0c315df278038a64e90e3444c31cf37c
cnknv <cankinav@gmail.com>
2020-05-03 23:01:43 -0500

    Initial Commit

---
+++ Rarefinder/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Rarefinder
+//
+//  Created by can kinav on 5/3/20.
+//  Copyright © 2020 John Kinav. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
